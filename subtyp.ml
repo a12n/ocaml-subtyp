d420766e83af6afaf9f01b44d503207c638f2b23
Anton Yabchinskiy <arn@bestmx.net>
2016-08-05 00:05:28 +0300

    Add endpoint constants

--- subtyp.ml
+++ subtyp.ml
@@ -1,26 +1,32 @@
 module Neg_int = struct
   type t = int
-  let of_int n = if n < 0 then n else invalid_arg "Neg_int.of_int"
+  let neg_one = -1
+  let of_int n = if n > neg_one then invalid_arg "Neg_int.of_int" else n
 end
 
 module Non_neg_float = struct
   type t = float
-  let of_float x = if x > 0.0 then x else invalid_arg "Non_neg_float.of_float"
+  let zero = 0.0
+  let of_float x = if x < zero then invalid_arg "Non_neg_float.of_float" else x
 end
 
 module Non_neg_int = struct
   type t = int
-  let of_int n = if n > -1 then n else invalid_arg "Non_neg_int.of_int"
+  let zero = 0
+  let of_int n = if n < zero then invalid_arg "Non_neg_int.of_int" else n
 end
 
 module Pos_int = struct
   type t = int
-  let of_int n = if n > 0 then n else invalid_arg "Pos_int.of_int"
+  let one = 1
+  let of_int n = if n < one then invalid_arg "Pos_int.of_int" else n
 end
 
 module Unit_float = struct
   type t = float
-  let of_float t = if t < 0.0 || t > 1.0 then invalid_arg "Unit_float.of_float" else t
+  let one = 1.0
+  let zero = 0.0
+  let of_float t = if t < zero || t > one then invalid_arg "Unit_float.of_float" else t
 end
 
 
--- subtyp.mli
+++ subtyp.mli
@@ -1,26 +1,32 @@
 module Neg_int : sig
   type t = private int
+  val neg_one : t
   val of_int : int -> t
 end
 
 module Non_neg_float : sig
   type t = private float
   val of_float : float -> t
+  val zero : t
 end
 
 module Non_neg_int : sig
   type t = private int
   val of_int : int -> t
+  val zero : t
 end
 
 module Pos_int : sig
   type t = private int
   val of_int : int -> t
+  val one : t
 end
 
 module Unit_float : sig
   type t = private float
   val of_float : float -> t
+  val one : t
+  val zero : t
 end
 
 
