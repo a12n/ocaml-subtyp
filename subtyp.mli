6565db50639cae1d8e7b6d1c0d94bf38f50afef0
Anton Yabchinskiy <arn@bestmx.net>
2016-08-05 00:06:15 +0300

    Add Non_empty_list module

--- subtyp.mli
+++ subtyp.mli
@@ -30,15 +30,27 @@ module Unit_float : sig
 end
 
 
+module Non_empty_list : sig
+  type 'a t = 'a * 'a list
+  val of_list : 'a list -> 'a t
+  val to_list : 'a t -> 'a list
+end
+
+
 module Std : sig
   type neg_int = Neg_int.t
   type non_neg_float = Non_neg_float.t
   type non_neg_int = Non_neg_int.t
   type pos_int = Pos_int.t
   type unit_float = Unit_float.t
+
+  type 'a non_empty_list = 'a Non_empty_list.t
+
   val neg_int : int -> neg_int
   val non_neg_float : float -> non_neg_float
   val non_neg_int : int -> non_neg_int
   val pos_int : int -> pos_int
   val unit_float : float -> unit_float
+
+  val non_empty_list : 'a list -> 'a non_empty_list
 end
