dba43508828adecdf7d5b6df4e5815254617f21e
Anton Yabchinskiy <arn@bestmx.net>
2021-11-12 09:58:19 +0300

    Remove Num related modules and functions

--- subtyp.ml
+++ subtyp.ml
@@ -16,12 +16,6 @@ module Non_neg_int = struct
   let of_int n = if n < zero then invalid_arg "Non_neg_int.of_int" else n
 end
 
-module Non_neg_num = struct
-  type t = Num.num
-  let zero = Num.Int 0
-  let of_num x = if Num.lt_num x zero then invalid_arg "Non_neg_num.of_num" else x
-end
-
 module Pos_int = struct
   type t = int
   let one = 1
@@ -33,23 +27,11 @@ module Pos_float = struct
   let of_float x = if x <= 0.0 then invalid_arg "Pos_float.of_float" else x
 end
 
-module Pos_num = struct
-  type t = Num.num
-  let of_num x = if Num.(le_num x (Int 0)) then invalid_arg "Pos_num.of_num" else x
-end
-
 module Unit_float = struct
   type t = float
   let one = 1.0
   let zero = 0.0
   let of_float t = if t < zero || t > one then invalid_arg "Unit_float.of_float" else t
-end
-
-module Unit_num = struct
-  type t = Num.num
-  let one = Num.Int 1
-  let zero = Num.Int 0
-  let of_num t = if Num.(lt_num t zero || gt_num t one) then invalid_arg "Unit_num.of_num" else t
 end
 
 
@@ -66,24 +48,18 @@ module Open = struct
   type neg_int = Neg_int.t
   type non_neg_float = Non_neg_float.t
   type non_neg_int = Non_neg_int.t
-  type non_neg_num = Non_neg_num.t
   type pos_float = Pos_float.t
   type pos_int = Pos_int.t
-  type pos_num = Pos_num.t
   type unit_float = Unit_float.t
-  type unit_num = Unit_num.t
 
   type 'a non_empty_list = 'a Non_empty_list.t
 
   let neg_int = Neg_int.of_int
   let non_neg_float = Non_neg_float.of_float
   let non_neg_int = Non_neg_int.of_int
-  let non_neg_num = Non_neg_num.of_num
   let pos_float = Pos_float.of_float
   let pos_int = Pos_int.of_int
-  let pos_num = Pos_num.of_num
   let unit_float = Unit_float.of_float
-  let unit_num = Unit_num.of_num
 
   let non_empty_list = Non_empty_list.of_list
 end
--- subtyp.mli
+++ subtyp.mli
@@ -16,12 +16,6 @@ module Non_neg_int : sig
   val zero : t
 end
 
-module Non_neg_num : sig
-  type t = private Num.num
-  val of_num : Num.num -> t
-  val zero : t
-end
-
 module Pos_int : sig
   type t = private int
   val of_int : int -> t
@@ -33,21 +27,9 @@ module Pos_float : sig
   val of_float : float -> t
 end
 
-module Pos_num : sig
-  type t = private Num.num
-  val of_num : Num.num -> t
-end
-
 module Unit_float : sig
   type t = private float
   val of_float : float -> t
-  val one : t
-  val zero : t
-end
-
-module Unit_num : sig
-  type t = private Num.num
-  val of_num : Num.num -> t
   val one : t
   val zero : t
 end
@@ -64,24 +46,18 @@ module Open : sig
   type neg_int = Neg_int.t
   type non_neg_float = Non_neg_float.t
   type non_neg_int = Non_neg_int.t
-  type non_neg_num = Non_neg_num.t
   type pos_float = Pos_float.t
   type pos_int = Pos_int.t
-  type pos_num = Pos_num.t
   type unit_float = Unit_float.t
-  type unit_num = Unit_num.t
 
   type 'a non_empty_list = 'a Non_empty_list.t
 
   val neg_int : int -> neg_int
   val non_neg_float : float -> non_neg_float
   val non_neg_int : int -> non_neg_int
-  val non_neg_num : Num.num -> non_neg_num
   val pos_float : float -> pos_float
   val pos_int : int -> pos_int
-  val pos_num : Num.num -> pos_num
   val unit_float : float -> unit_float
-  val unit_num : Num.num -> unit_num
 
   val non_empty_list : 'a list -> 'a non_empty_list
 end
